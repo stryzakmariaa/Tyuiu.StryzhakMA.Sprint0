5145574698e890258604a37a69383cdc48524299
Стрыжак Мария < @LAPTOP-STRYZHAK>
2026-09-10 22:33:55 +0500

    Task1

---
+++ Tyuiu.StryzhakMA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.StryzhakMA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Мария";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
