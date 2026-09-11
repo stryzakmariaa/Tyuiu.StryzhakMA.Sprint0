e37e0b0b4d2a8f1d1eb41920ee70185fd1ce1774
Стрыжак Мария < @LAPTOP-STRYZHAK>
2026-09-11 21:33:55 +0500

    Task2

---
+++ Tyuiu.StryzhakMA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.StryzhakMA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.StryzhakMA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Мария";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Мария", res);
+        }
+    }
+}
---
+++ Tyuiu.StryzhakMA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.StryzhakMA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.StryzhakMA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вызов класса DataService и метода GetMessage
+            //из библиотеки Tyuiu.StryzhakMA.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Мария"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
