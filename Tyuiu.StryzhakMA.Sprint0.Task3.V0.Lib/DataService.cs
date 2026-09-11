64440aea582169c1de6c5f869968624e54f02557
Стрыжак Мария < @LAPTOP-STRYZHAK>
2026-09-12 01:31:14 +0500

    Task3

---
+++ Tyuiu.StryzhakMA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.StryzhakMA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        //Реализация метода Sum
+        public static int Sum(int a, int b)
+        {  
+            return a + b; 
+        }
+    }
+}
---
+++ Tyuiu.StryzhakMA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.StryzhakMA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.StryzhakMA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
