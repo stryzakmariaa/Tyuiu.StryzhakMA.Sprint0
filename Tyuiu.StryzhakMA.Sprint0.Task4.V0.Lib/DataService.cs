ed0c176b25d7ed14f81a0ad0bb0ac33c00a60931
Стрыжак Мария < @LAPTOP-STRYZHAK>
2026-09-13 12:01:54 +0500

    Task4

---
+++ Tyuiu.StryzhakMA.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿namespace Tyuiu.StryzhakMA.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        //Пример линейной структуры 
+        public static int Addition(int a, int b)
+        {  
+            return a + b; 
+        }
+
+        //Пример линейной структуры
+        public static int Subtracation(int a, int b)
+        {  
+            return a - b; 
+        }
+
+        //Пример линейной структуры
+        public static int Multiplication(int a, int b) 
+        { 
+            return a * b; 
+        }
+
+        //Пример линейной структуры
+        public static int Division(int a, int b) 
+        { 
+            return a / b; 
+        }
+    }
+}
---
+++ Tyuiu.StryzhakMA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.StryzhakMA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.StryzhakMA.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры программы
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+            //Вызов метода вычитания Subtracation
+            Console.WriteLine(DataService.Subtracation(15, 5));
+            //Вызов метода умножения Multiplication
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            //Вызов метода деления Division
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
