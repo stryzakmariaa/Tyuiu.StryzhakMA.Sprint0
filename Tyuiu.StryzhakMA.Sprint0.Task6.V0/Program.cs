512afaf2878aaf984d0658464246263ee10f550f
Стрыжак Мария < @LAPTOP-STRYZHAK>
2026-09-13 20:24:15 +0500

    Task6

--- Tyuiu.StryzhakMA.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.StryzhakMA.Sprint0.Task6.V0/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Tyuiu.StryzhakMA.Sprint0.Task6.V0
+﻿using Tyuiu.StryzhakMA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.StryzhakMA.Sprint0.Task6.V0
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] numsArray = new int { 1, 2, 3, 4, 5 };
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
             //Пример циклической структуры находится в библиотеке классов в методе AdditionArray
             Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
 
@@ -12,7 +14,7 @@
             Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
 
             //Пример циклической структуры находится в библиотеке классов в методе MultiplicationArray
-            Console.WriteLine("Произведение элементвой массива =" + DataService.MultiplicationArray(numsArray));
+            Console.WriteLine("Произведение элементов массива =" + DataService.MultiplicationArray(numsArray));
 
             Console.ReadKey();
         }
