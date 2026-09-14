1c490f38882e513d32ac38d56a19c4df26744c9a
Стрыжак Мария < @LAPTOP-STRYZHAK>
2026-09-14 19:34:00 +0500

    Task7

---
+++ Tyuiu.StryzhakMA.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.StryzhakMA.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        //Пример циклической структуры (цикл с параметром) for
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < numOne.Length; i++ )
+            {
+                resultArray[i] = numOne[i] + numTwo[i];
+            }
+
+            return resultArray;
+        }
+    }
+}
---
+++ Tyuiu.StryzhakMA.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,65 @@
+﻿using Tyuiu.StryzhakMA.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.StryzhakMA.Sprint0.Task7.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнила: Стрыжак М. А. | АСОиУб-26-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнила: Стрыжак Мария Андреевна | АСОиУб-26-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ :                                                               *");
+            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения двух    *");
+            Console.WriteLine("* одинаковых массивов по длине.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            //Место решения задания
+            int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5, };
+
+            Console.WriteLine("Значение элементов массива №1");
+            for (int i = 0; i < arrayNums1.Length; i++)
+            {
+                Console.WriteLine(arrayNums1[i] + ", ");
+            }
+
+            Console.WriteLine();
+
+            int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Значение элементов массива №2");
+            for (int i = 0;i < arrayNums2.Length; i++)
+            {
+                Console.WriteLine(arrayNums2[i] + ", ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ :                                                             *");
+            Console.WriteLine("***************************************************************************");
+            if (arrayNums1.Length == arrayNums2.Length)
+            {
+                int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
+
+                Console.WriteLine("Сумма элементов массива равна:");
+                for (int i = 0; i < resultArray.Length; i++)
+                {
+                    Console.WriteLine(resultArray[i] + ", ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Исходные массивы имеют разное количество элементов");
+            }
+            Console.ReadKey();
+        }
+    }
+}
